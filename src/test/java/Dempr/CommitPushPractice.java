f458688328bf353d6ee3d9e16776917ab8793466
djs <djs@DESKTOP-5MTUOF7>
2024-01-25 17:08:28 +0530

    Demo Committed

---
+++ src/test/java/Dempr/CommitPushPractice.java
@@ -0,0 +1,15 @@
+package Dempr;
+
+public class CommitPushPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("GoodMorning");
+		System.out.println("C="+c);
+		System.out.println("Good");
+	}
+
+}
